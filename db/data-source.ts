3f56db05a88b9a8552e7a18d9886bd0d7fc086e1
olliso17 <olli.so17@gmail.com>
2024-06-16 13:25:33 -0300

    update package json

--- db/data-source.ts
+++ db/data-source.ts
@@ -2,11 +2,11 @@ import { DataSource, DataSourceOptions } from "typeorm";
 
 export const dataSourceOptions: DataSourceOptions = {
   type: "postgres",
-  host: "localhost",
-  port: 5432,
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  database: "postgres",
+  database:process.env.POSTGRES_DATABASE,
   entities: ["dist/**/*.entity.js"],
   migrations: ["dist/db/migrations/*.js"],
   synchronize: true,   /*quando for sincronizar com o banco*/
